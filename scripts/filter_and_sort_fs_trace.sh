a99729900f4237791eca491ab4be438404993d7c
Patrick Maia <patrickjourdanmaia@gmail.com>
2011-10-26 23:07:16 -0300

    a script to sort events in time ascending order

---
+++ scripts/filter_and_sort_fs_trace.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# 1. filter only events of interest from the raw trace
+# 2. sort the events in time ascending order
+
+grep -P "sys_[open|close|write|read|unlink]" | sort -n -k 6,6.17
