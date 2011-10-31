de8542f7ece732495a0c467fbaa3d66e03adbc62
Patrick Maia <patrickjourdanmaia@gmail.com>
2011-10-31 20:58:36 -0300

    after pre-processor trace has no opens anymore

--- scripts/clean_fs_trace.py
+++ scripts/clean_fs_trace.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
 
-# 1. keep only the calls to open, close, read, write and unlink
+# 1. keep only the calls to close, read, write and unlink
 # 2. remove all unnecessary information like user id, process id, thread id, open flag, etc
 
 import sys
 
 global_opened_files_map = dict()
-close_without_open_count = 0
-read_without_open_count = 0
-write_without_open_count = 0
 bad_format_open = 0
 bad_format_close = 0
 bad_format_read = 0
@@ -18,35 +15,26 @@ bad_format_unlink = 0
 # line sample from the original trace
 #	uid pid tid exec_name sys_open begin-elapsed cwd filename flags mode return
 #	0 2097 2097 (udisks-daemon) sys_open 1318539063003892-2505 / /dev/sdb 34816 0 7
-# line transformed by clean_open
-#	open	begin-elapsed	fullpath	fd-pid
-#	open	1318539063003892-2505	/dev/sdb	7-2097
-def clean_open(tokens):
+def open_file(tokens):
 	if len(tokens) != 11:
 		global bad_format_open
 		bad_format_open = bad_format_open + 1
-		return None;
-
-	if not tokens[7].startswith('/'):
-		fullpath = tokens[6] + tokens[7]
 	else:
-		fullpath = tokens[7]
-
-	unique_file_id =  tokens[10] + '-' + tokens[1]
-
-	global_opened_files_map[unique_file_id] = fullpath 
-
-	if not fullpath.startswith("/home"):
-		return None
-	else:
-		return "\t".join(['open', tokens[5], fullpath, unique_file_id])
+		if not tokens[7].startswith('/'):
+			fullpath = tokens[6] + tokens[7]
+		else:
+			fullpath = tokens[7]
+	
+		unique_file_id =  tokens[10] + '-' + tokens[1]
+	
+		global_opened_files_map[unique_file_id] = fullpath 
 
 # line sample from the original trace
 #	uid pid tid exec_name sys_close begin-elapsed fd return
 #	0 2097 2097 (udisks-daemon) sys_close 1318539063006403-37 7 0
 # line transformed by clean_close
-#	close	begin-elapsed	fd-pid
-#	close	1318539063006403-37	7-2097
+#	close	begin-elapsed	fullpath
+#	close	1318539063006403-37	/local/userActivityTracker/logs/tracker.log/
 def clean_close(tokens):
 	if len(tokens) != 8:
 		global bad_format_close
@@ -56,68 +44,67 @@ def clean_close(tokens):
 	unique_file_id = tokens[6] + '-' + tokens[1]
 	fullpath = global_opened_files_map.get(unique_file_id, None)
 
-	if fullpath == None:
-		global close_without_open_count
-		close_without_open_count = close_without_open_count + 1
-
-		return None
-	else:
+	if fullpath != None:
 		del global_opened_files_map[unique_file_id]
 
 		if not fullpath.startswith("/home"):
 			return None
 		else:
-			return "\t".join(['close', tokens[5], unique_file_id])
+			return "\t".join(['close', tokens[5], fullpath])
 
 # line sample from the original trace: 
 #	uid pid tid exec_name sys_write begin-elapsed (root pwd fullpath f_size f_type ino) fd count return
 #	0 6194 6194 (xprintidle) sys_write 1318539063058255-131 (/ /root/ /local/userActivityTracker/logs/tracker.log/ 10041417 S_IFREG|S_IROTH|S_IRGRP|S_IWUSR|S_IRUSR 2261065) 1 17 17
 # line transformed by clean_write
-#	write	begin-elapsed	fd-pid	length
-#	write	1318539063058255-131	1-6194	17
+#	write	begin-elapsed	fullpath	length
+#	write	1318539063058255-131	/local/userActivityTracker/logs/tracker.log/	17
 def clean_write(tokens):
 	if len(tokens) != 15:
 		global bad_format_write
 		bad_format_write = bad_format_write + 1
-		return None;
-
-	unique_file_id = tokens[12] + '-' + tokens[1]
-
-	if not global_opened_files_map.has_key(unique_file_id):
-		global write_without_open_count
-		write_without_open_count = write_without_open_count + 1
-
-		return None
+		unique_file_id = tokens[6] + '-' + tokens[1]
+		fullpath = global_opened_files_map.get(unique_file_id, None)
+		length = tokens[8]
 	else:
-		if not tokens[8].startswith("/home"):
+		unique_file_id = tokens[12] + '-' + tokens[1]
+		fullpath = tokens[8]
+		length = tokens[14]
+
+	if fullpath != None:
+		global_opened_files_map[unique_file_id] = fullpath
+		if not fullpath.startswith("/home"):
 			return None
 		else:
-			return "\t".join(['write', tokens[5], unique_file_id, tokens[14]])
+			return "\t".join(['write', tokens[5], fullpath, length])
+	else:
+		return None
 
 # line sample from the original trace: 
 #	uid pid tid exec_name sys_read begin-elapsed (root pwd fullpath f_size f_type ino) fd count return
 #	114 1562 1562 (snmpd) sys_read 1318539063447564-329 (/ / /proc/stat/ 0 S_IFREG|S_IROTH|S_IRGRP|S_IRUSR 4026531975) 8 3072 2971
 # line transformed by clean_read
-#	read	begin-elapsed	fd-pid	length
-#	read	1318539063447564-329	8-1562	2971
+#	read	begin-elapsed	fullpath	length
+#	read	1318539063447564-329	/proc/stat/	2971
 def clean_read(tokens):
 	if len(tokens) != 15:
 		global bad_format_read
 		bad_format_read = bad_format_read + 1
-		return None;
-
-	unique_file_id = tokens[12] + '-' + tokens[1]
-
-	if not global_opened_files_map.has_key(unique_file_id):
-		global read_without_open_count
-		read_without_open_count = read_without_open_count + 1
-
-		return None
+		unique_file_id = tokens[6] + '-' + tokens[1]
+		fullpath = global_opened_files_map.get(unique_file_id, None)
+		length = tokens[8]
 	else:
-		if not tokens[8].startswith("/home"):
+		unique_file_id = tokens[12] + '-' + tokens[1]
+		fullpath = tokens[8]
+		length = tokens[14]
+
+	if fullpath != None:
+ 		global_opened_files_map[unique_file_id] = fullpath
+		if not fullpath.startswith("/home"):
 			return None
 		else:
-			return "\t".join(['read', tokens[5], unique_file_id, tokens[14]])
+			return "\t".join(['read', tokens[5], fullpath, length])
+	else:
+		return None
 
 # line sample from the original trace: 
 #	uid pid tid exec_name sys_unlink begin-elapsed cwd pathname return
@@ -148,7 +135,7 @@ for line in sys.stdin:
 
 	clean_line = None
 	if tokens[4] == 'sys_open':
-		clean_line = clean_open(tokens)
+		clean_line = open_file(tokens)
 	elif tokens[4] == 'sys_close':
 		clean_line = clean_close(tokens)
 	elif tokens[4] == 'sys_write':
@@ -161,10 +148,6 @@ for line in sys.stdin:
 	if clean_line != None:
 		print clean_line
 
-print '# number of readings, writings and closings without preceding openings'
-print '# closes:\t' + str(close_without_open_count)
-print '# reads:\t' + str(read_without_open_count)
-print '# writes:\t' + str(write_without_open_count)
 print '# number of bad formatted reads, writes, opens, closes and unlinks'
 print '# reads:\t' + str(bad_format_read)
 print '# writes:\t' + str(bad_format_write)
--- scripts/filter_and_sort_fs_trace.sh
+++ scripts/filter_and_sort_fs_trace.sh
@@ -3,4 +3,4 @@
 # 1. filter only events of interest from the raw trace
 # 2. sort the events in time ascending order
 
-grep -P "sys_[close|write|read|unlink]" | sort -n -k 6,6.17
+grep -P "sys_[open|close|write|read|unlink]+" | grep -v "S_IFSOCK" | sort -n -k 6,6.17
