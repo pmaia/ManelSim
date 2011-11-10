15d595e0b8f091ac0151dccf1207c01339afa742
Patrick Maia <patrickjourdanmaia@gmail.com>
2011-11-10 14:35:48 -0300

    filter_fs_trace in python

--- scripts/filter_fs_trace.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# 1. removes most of the events that are not of interest of the simulation
-
-grep -P -v "S_IFIFO|S_IFSOCK|S_IFBLK|S_IFCHR" | grep -P "sys_(open|close|write|read|unlink)|do_filp_open" 
