400d55779d704059afa9984398ac51ad98a1f137
Patrick Maia <patrickjourdanmaia@gmail.com>
2011-11-10 14:40:37 -0300

    filter is in python now... updating maestro

--- scripts/maestro.sh
+++ scripts/maestro.sh
@@ -7,5 +7,5 @@ traces_files=`ls $traces_dir | grep  "log"`
 
 for trace_file in $traces_files 
 do
-	cat $traces_dir/$trace_file | ./filter_fs_trace.sh >> $output_file
+	cat $traces_dir/$trace_file | ./filter_fs_trace.py >> $output_file
 done
