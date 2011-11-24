20000e6447c8ebe437b13706ea1a61e8ee71ddb4
Patrick Maia <patrickjourdanmaia@gmail.com>
2011-11-23 23:23:23 -0300

    gerere gerere lsd

--- scripts/clean_idleness_trace.py
+++ scripts/clean_idleness_trace.py
@@ -6,6 +6,7 @@ line = sys.stdin.readline()
 tokens = line.split()
 previous_timestamp = int(tokens[0])
 previous_idleness_time = int(tokens[1]) / 1000
+pending_shutdown = False
 
 for line in sys.stdin:
 	tokens = line.split()
@@ -15,28 +16,31 @@ for line in sys.stdin:
 	time_between_logs = timestamp - previous_timestamp
 
 	if time_between_logs > 1:  
-	#the tracker for some reason didn't log during some seconds. 
-	#we need to decide if the machine was sleeping (shutdown) or just idle during this time. 
-	#we consider just these two states because they are the pessimistic scenarios for the hypothesis we think is true
-		if time_between_logs < 6 * 60:
-		#there was a cron job that should initialize the idleness tracker every 5 minutes in case it was not still running.
-		#because of that, we think it is reasonable to consider that in case of the gap duration is less than 6 minutes, 
-		#the machine was on but the tracker was not or it was on but restarting. So, lets adjust things to look like a normal idleness...
-			previous_timestamp = timestamp - 1
-			previous_idleness_time = previous_idleness_time + time_between_logs - 1
-			
-			if idleness_time < previous_idleness_time:
-				print "idleness\t" + str(previous_timestamp - previous_idleness_time) + "\t" + str(previous_idleness_time + time_between_logs)
+	# The tracker for some reason didn't log during some seconds. 
+	# We need to decide if the machine was turned off or just idle during this time. 
+	# we consider just these two states because they are the pessimistic scenarios for the hypothesis we think is true.
+	#
+	# During the time the trace was being collected there was a cron job that should initialize the idleness tracker 
+	# at most after 5 minutes from the last check.
+	# Because of that, we think it is reasonable to consider that in case of the gap duration is less than 6 minutes, 
+	# the machine was on but the tracker was not or it was on but restarting. 
+	# So, lets adjust things to look like a normal idleness in this case...
 
-		else:
-		#the possibility that idleness_time is a continuation of previous_idleness_time still remains. we need to check
-			if idleness_time - previous_idleness_time <= time_between_logs:
-				print "shutdown\t" + str(previous_timestamp + previous_idleness_time) + "\t" + str(time_between_logs - idleness_time)
-#			else:
-#				do nothing, it is a continuation of a previous idleness period (I'm not sure if this is true)
+		previous_timestamp = timestamp - 1
+		previous_idleness_time = previous_idleness_time + time_between_logs - 1
+
+		if time_between_logs > 6 * 60: # and if the gap is too big, we consider it as a shutdown
+			pending_shutdown = True			
 	
-	elif idleness_time < previous_idleness_time:
-		print "idleness\t" + str(previous_timestamp - previous_idleness_time) + "\t" + str(previous_idleness_time + time_between_logs)
+	if idleness_time < previous_idleness_time:
+		start =  str(previous_timestamp - previous_idleness_time) 
+		duration = str(previous_idleness_time + time_between_logs)
+
+		if pending_shutdown:
+			print "shutdown\t" + start + "\t" + duration
+			pending_shutdown = False
+		else:
+			print "idleness\t" + start + "\t" + duration
 
 	previous_timestamp = timestamp
 	previous_idleness_time = idleness_time
