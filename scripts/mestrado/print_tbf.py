0ffc4ee2f6107157fa09d39f27ec00bbe8803185
Patrick Maia <patrickjourdanmaia@gmail.com>
2012-03-16 09:58:35 -0300

    redmine issue 1542

--- scripts/mestrado/print_tbf.py
+++ scripts/mestrado/print_tbf.py
@@ -2,22 +2,26 @@
 
 import sys
 
+if len(sys.argv) < 3:
+	print 'usage: ' + sys.argv[0] + ' <machine_id> <machine_name>'
+	sys.exit(1)
+
+
 previous_line = sys.stdin.readline()
 
-last_failure = None
+reliable_ts = None
+failed = False
 
-def report_failure():
-	global last_failure
-	print str(previous_ts - last_failure)
-	last_failure = current_ts
+machine_id = sys.argv[1]
+machine_name = sys.argv[2]
 
 for line in sys.stdin:
 
 	previous_ts = int(previous_line.split()[0])
 	prev_idleness = int(previous_line.split()[1]) / 1000
 
-	if last_failure == None:
-		last_failure = previous_ts
+	if reliable_ts == None:
+		reliable_ts = previous_ts
 
 	current_ts = int(line.split()[0])
 	cur_idleness = int(line.split()[1]) / 1000
@@ -25,17 +29,23 @@ for line in sys.stdin:
 	ts_diff = current_ts - previous_ts
 
 	if ts_diff <= 0:
-		report_failure()
+		failed = True
 	elif ts_diff > 1:
-		report_failure()
+		failed = True
 	
 	if ts_diff >= 1:
 		if cur_idleness < prev_idleness:
 			if cur_idleness > ts_diff:
-				report_failure()
-		elif cur_idleness > (prev_idleness + ts_diff):
-				report_failure()
+				failed = True
+		elif cur_idleness > (prev_idleness + ts_diff + 1):
+				failed = True
 	
-	previous_line = line
+	if failed:
+		print str(reliable_ts) + '\t' + machine_id + '\t' + machine_name
+	else:
+		print str(reliable_ts) + '\t0\t' + machine_name
 
+	previous_line = line
+	failed = False
+	reliable_ts = reliable_ts + 1
 
