120801083827ce429534e1e10d9f4068157088b9
Patrick Maia <patrickjourdanmaia@gmail.com>
2012-04-17 18:01:45 -0300

    when filtering traces filter_fs_traces.sh is not putting all output together anymore

--- scripts/mestrado/fs/filter_fs_traces.sh
+++ scripts/mestrado/fs/filter_fs_traces.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 traces_dir=$1
-output_file=$2
+output_dir=$2
 
 traces_files=`ls $traces_dir | grep  "log"`
 
 for trace_file in $traces_files 
 do
-	cat $traces_dir/$trace_file | ./filter_fs_trace.py >> $output_file
+	cat $traces_dir/$trace_file | ./filter_fs_trace.py > $output_dir/$trace_file.filtered
 done
