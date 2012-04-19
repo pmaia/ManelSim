22b39b674eec30fb4fd626bf0bc4a6665edee143
Patrick Maia <patrickjourdanmaia@gmail.com>
2012-04-19 11:11:02 -0300

    filter_fs_traces.sh improved

--- scripts/mestrado/fs/filter_fs_traces.sh
+++ scripts/mestrado/fs/filter_fs_traces.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+machines="abelhinha celacanto charroco cherne gupi jurupoca mulato ourico palmito pepino pimpim roncador sargento traira viola"
+
 traces_dir=$1
 output_dir=$2
 
-traces_files=`ls $traces_dir | grep  "log"`
-
-for trace_file in $traces_files 
+for machine in $machines
 do
-	cat $traces_dir/$trace_file | ./filter_fs_trace.py > $output_dir/$trace_file.filtered
+	machine_traces_dir=$traces_dir/$machine
+
+	if [ -d $machine_traces_dir -a ! -d $output_dir/$machine-filtered ]; then
+		echo "Filtering $machine"
+
+		mkdir $output_dir/$machine-filtered
+
+		traces_files=`ls $machine_traces_dir | grep  "log"`
+
+		for trace_file in $traces_files 
+		do
+			cat $machine_traces_dir/$trace_file | ./filter_fs_trace.py > $output_dir/$machine-filtered/$trace_file.filtered
+		done
+	else
+		echo "Skipping $machine"
+	fi
 done
