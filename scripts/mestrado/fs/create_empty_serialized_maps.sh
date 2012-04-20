cc6d0eb9a9cf1e00043fca9638264fc72347733d
Patrick Maia <patrickjourdanmaia@gmail.com>
2012-04-20 13:13:55 -0300

    introducing create_empty_serialized_maps.sh

---
+++ scripts/mestrado/fs/create_empty_serialized_maps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+machines="abelhinha celacanto charroco cherne gupi jurupoca mulato ourico palmito pepino pimpim roncador sargento traira viola"
+maps="# fdpid_to_fullpath&# fullpath_to_filetype&# fullpath_to_filesize"
+
+IFS='&'
+
+output_dir=$1
+
+for machine in $machines
+do
+	for map in $maps
+	do
+		echo $map >> $output_dir/$machine-aux-maps-in
+	done
+done
+
