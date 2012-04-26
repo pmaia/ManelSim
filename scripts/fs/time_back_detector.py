6c072e3b2b31afbe8ae8a7b6e677fd646e4963bd
Patrick Maia <patrickjourdanmaia@gmail.com>
2012-04-26 17:03:14 -0300

    time back detector

--- scripts/fs/time_back_detector.py
+++ scripts/fs/time_back_detector.py
@@ -2,12 +2,19 @@
 
 import sys
 
-prev_timestamp = int(sys.stdin.readline()[5))
-line_count = 1
-for line in sys.stdin:
-	line_count = line_count + 1
-	timestamp = int(line.split()[5])
+def extract_time(line):
+	time_str = line.split()[1].split("-")[0]
+	return int(time)
 
-	if timestamp < prev_timestamp:
-		print "back in time. line " + str(line_count)
+def main():
+	prev_timestamp = extract_time(sys.stdin.readline())
+	line_count = 1
+	for line in sys.stdin:
+		line_count += 1
+		timestamp = extract_time(line) 
 
+		if timestamp < prev_timestamp:
+			print "back in time. line " + str(line_count)
+
+if __name__ == "__main__":
+	main()
