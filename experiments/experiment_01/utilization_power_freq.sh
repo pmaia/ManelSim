1037f5a185c8d7da2050403eb5968d80f073d8ad
Patrick Maia <patrickjourdanmaia@gmail.com>
2012-12-02 00:36:01 -0300

    #7 - removed config files and scripts related to beefs energy experiment

--- experiments/experiment_01/utilization_power_freq.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-while true; do
-	date +%s
-	mpstat 1 1
-	cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq
-	cat /proc/acpi/battery/BAT0/state
-done
--- scripts/fs/sort_fs_trace.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 1. sort the events in time ascending order
-
-TEMP_DIR=$1
-
-sort -T $TEMP_DIR -n -k 6,6.17
